9d065b3eee862277750a1850915c196e0a417a59
salahuddin-rakib <salahuddin.rakib.uiu@gmail.com>
2020-10-18 02:57:50 +0600

    modified seeds.rb file from db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,5 +68,5 @@ Product.create!(title: 'Programming Crystal',
       tooling. Now you can write beautiful code that runs faster, scales better, 
       and is a breeze to deploy.
       </p>},
-  image_url: 'crystal.jpg',
+  image_url: 'funny.jpg',
   price: 40.00)
