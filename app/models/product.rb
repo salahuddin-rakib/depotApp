7f22ad5b5d83827be3c2390f7e9379ca6f66bb11
salahuddin-rakib <salahuddin.rakib.uiu@gmail.com>
2020-10-20 11:45:58 +0600

    added Unit testing of Models with Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,21 @@
+=begin
+It was showing almost 5 errors. Find the reason why it was showing that much error.
+
+class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :image, allow_blank: true, format: {
+      with: %r{.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
+  validates :price, numericality: { greater_than_or_equal_to: 1.00 }
+end
+=end
+
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif/jpg/png)\z}i,
-    message: 'must be a url for GIF, JPG or PNG image.' 
+      with: %r{\.(gif|jpg|png)\z}i,
+      message: 'must be a URL for GIF, JPG or PNG image.'
   }
-  validates :price, numericality: { greater_than_or_equal_to: 1.00 }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
